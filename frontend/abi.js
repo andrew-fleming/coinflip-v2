2219ed78204c66ad456bd823816d9817a767796d
Andrew <fleming-andrew@protonmail.com>
2021-04-26 00:40:45 -0400

    Added vanilla html/js frontend with operational buttons

---
+++ frontend/abi.js
@@ -0,0 +1,313 @@
+window.abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_coorAddr",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "_linkAddr",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "_keyHash",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_fee",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "player",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "choice",
+          "type": "uint256"
+        }
+      ],
+      "name": "BetInit",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "player",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "Outcome",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "player",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "Withdraw",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_recipient",
+          "type": "address"
+        }
+      ],
+      "name": "addPlayerWinnings",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "name": "afterWaiting",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "contractBalance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_choice",
+          "type": "uint256"
+        }
+      ],
+      "name": "flip",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getLinkBalance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getTotalValue",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getWinningsBalance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "playerWinnings",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "requestId",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "randomness",
+          "type": "uint256"
+        }
+      ],
+      "name": "rawFulfillRandomness",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "waiting",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "playerAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "requestId",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "betAmount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "headsTails",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdrawContract",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdrawLink",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdrawPlayerWinnings",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+  
---
+++ frontend/main.js
@@ -0,0 +1,128 @@
+//Moralis.initialize("Qmat4iPy7apkfHvGjUuZ46ZO0qPcXgYGW42fthON")
+//Moralis.serverURL = "https://csqg51vkg0kz.moralis.io:2053/server"
+//const CONTRACT_ADDRESS = "0xB9C7b15aB1BC2A49f09142F815a2265459E9c028"
+Moralis.initialize("<INSERT_API_KEY_HERE")
+Moralis.serverURL = "<INSERT_SERVER_URL_HERE"
+const CONTRACT_ADDRESS = "<INSERT_CONTRACT_ADDRESS_HERE>"
+
+function fromWei(num){
+    return num / (10 ** 18)
+}
+
+function toWei(num){
+    return num * (10**18)
+}
+
+async function renderApp() {
+    /**
+     * Instantiates Web3 and contract. This is called by the login function.
+     */
+    window.web3 = await Moralis.Web3.enable();
+    window.contract = new web3.eth.Contract(window.abi, CONTRACT_ADDRESS);
+    console.log(window.contract)
+}
+
+
+async function login() {
+    /**
+     * Opens the application upon connecting to MetaMask.
+     */
+    try {
+        user = await Moralis.User.current();
+        if(!user) {
+            user = await Moralis.Web3.authenticate();
+        }
+
+        console.log(user);
+        document.getElementById("login_button").style.display = "none";
+        document.getElementById("game").style.display = "block";
+        renderApp()
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function flip(choice) {
+    /**
+     * @notice Core function of this dApp. 
+     * @param {*} choice Heads or tails where heads is zero and tails is one. 
+     */
+    let amount = document.getElementById("amount").value
+    window.web3 = await Moralis.Web3.enable()
+    let config = {
+        value: toWei(amount),
+        from: ethereum.selectedAddress
+    }
+    window.contract.methods.flip(choice).send(config)
+    .on('receipt', function(receipt){
+        console.log(receipt)
+    })
+}
+
+async function withdrawPlayerWinnings(){
+    /**
+     * @notice
+     */
+    let config = {
+        from: ethereum.selectedAddress,
+    }
+    window.contract.methods.withdrawPlayerWinnings().send(config)
+    .on("receipt", function(receipt){
+        console.log(receipt)
+    })
+}
+
+
+/// ***** GETTERS ***** ///
+
+async function getContract(){
+    /**
+     * @notice
+     */
+    let bal = await window.contract.methods.contractBalance().call()
+    console.log(`Contract Balance: ${fromWei(bal)} ETH`)
+}
+
+async function getWinningsBalance(){
+    /**
+     * @notice Fetches the caller's balance.
+     */
+    config = {
+        from: ethereum.selectedAddress
+    }
+    let bal = await window.contract.methods.getWinningsBalance().call(config)
+    console.log(`Winnings balance: ${fromWei(bal)} ETH`)
+}
+
+async function getTotalValue(){
+    /**
+     * @notice Owner function. Fetches both the total balance of the contract as well as
+     * the combined player winning's balances.
+     */
+    config = {
+        from: ethereum.selectedAddress
+    }
+    let bal = await window.contract.methods.getTotalValue().call(config)
+    console.log(`Total value: ${fromWei(bal)} ETH`)
+}
+
+async function getLinkBalance(){
+    /**
+     * Owner function
+     */
+    config = {
+        from: ethereum.selectedAddress
+    }
+    let bal = await window.contract.methods.getLinkBalance().call(config)
+    console.log(`Link balance: ${fromWei(bal)} LINK`)
+}
+
+document.getElementById("login_button").onclick = login;
+document.getElementById("heads_button").onclick = function(){flip(0)};
+document.getElementById("tails_button").onclick = function(){flip(1)};
+document.getElementById("withdraw_player_winnings").onclick = function(){withdrawPlayerWinnings()}
+document.getElementById("contract_balance").onclick = function(){getContract()}
+document.getElementById("player_winnings").onclick = function(){getWinningsBalance()};
+document.getElementById("link_balance").onclick = function(){getLinkBalance()};
+document.getElementById("total_value").onclick = function(){getTotalValue()}
